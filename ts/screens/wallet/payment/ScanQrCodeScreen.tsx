01b11bbd4d045cf41d39c0aedccfeb35e2284cec
Edigio <egidio.filosa@gmail.com>
2019-05-23 17:17:57 +0200

    [#165632270] pick QRcode from gallery
    Pick and scan QRcode from an image stored on the device

--- ts/screens/wallet/payment/ScanQrCodeScreen.tsx
+++ ts/screens/wallet/payment/ScanQrCodeScreen.tsx
@@ -22,6 +22,8 @@ import I18n from "../../../i18n";
 import { Dispatch } from "../../../store/actions/types";
 import { ComponentProps } from "../../../types/react";
 
+import ImagePicker from "react-native-image-picker";
+import * as ReaderQR from "react-native-lewin-qrcode";
 import { BaseHeader } from "../../../components/screens/BaseHeader";
 import FooterWithButtons from "../../../components/ui/FooterWithButtons";
 import { CameraMarker } from "../../../components/wallet/CameraMarker";
@@ -63,6 +65,12 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     justifyContent: "center",
     backgroundColor: "transparent"
+  },
+
+  button: {
+    alignContent: "center",
+    justifyContent: "center",
+    alignSelf: "center"
   },
 
   camera: {
@@ -135,6 +143,28 @@ class ScanQrCodeScreen extends React.Component<Props, State> {
   private onQrCodeData = (data: string) => {
     const resultOrError = decodePagoPaQrCode(data);
     resultOrError.foldL<void>(this.onInvalidQrCode, this.onValidQrCode);
+  };
+
+  private showImageLib = () => {
+    const options = {
+      storageOptions: {
+        skipBackup: true,
+        path: "images"
+      }
+    };
+    // Open Image Library:
+    ImagePicker.launchImageLibrary(options, response => {
+      const path = response.path ? response.path : response.uri;
+      if (path != null) {
+        ReaderQR.readerQR(path)
+          .then((data: string) => {
+            this.onQrCodeData(data);
+          })
+          .catch(() => {
+            this.onInvalidQrCode();
+          });
+      }
+    });
   };
 
   public constructor(props: Props) {
@@ -211,6 +241,10 @@ class ScanQrCodeScreen extends React.Component<Props, State> {
                   <Text style={[styles.padded, styles.centerText]}>
                     {I18n.t("wallet.QRtoPay.cameraUsageInfo")}
                   </Text>
+                  <View spacer={true} />
+                  <Button onPress={this.showImageLib} style={styles.button}>
+                    <Text>{I18n.t("wallet.QRtoPay.chooser")}</Text>
+                  </Button>
                   <View spacer={true} extralarge={true} />
                 </View>
               }
